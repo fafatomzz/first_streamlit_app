ee73963c219ac2d93a61f38ca4206013f4be3198
Tommie Zabala <tommie.zabala@gmail.com>
2023-06-16 18:43:08 +0800

    Try and Catch Conversion

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,9 +40,6 @@ try:
         back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
 
-except URLError as e:
-    streamlit.error()
-
 streamlit.stop()
 
 #import snowflake.connector
